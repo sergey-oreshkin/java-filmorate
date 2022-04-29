cd8c1868eb6f14f5010a0bb3f8d94e7615724966
sergey-oreshkin <joknife79@yandex.ru>
2022-04-29 22:14:17 +0500

    remove spring validation

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -2,10 +2,7 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-
 @Data
-@NotNull
 public class Film {
     private int id;
     private String name;
